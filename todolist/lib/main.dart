0d3419473732bbf801f26289d834d60e93007b70
jalala-pv <pvjalala@gmail.com>
2023-02-17 14:06:12 +0530

    dd

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp();
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -16,29 +17,54 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home:TodoListPage(),
+      home: TodoListPage(),
     );
   }
 }
-class TodoListPage extends StatelessWidget {
-  
 
+class TodoListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final width=MediaQuery.of(context).size.width;
+    final height=MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
         backgroundColor: Colors.grey[300],
-        leading: Icon(Icons.menu,color: Colors.black),
-        actions: [Image.asset('assets/avatar.png',width: 40,height: 40,)],
-        
-      ),
-    body: Container(
-      color: Colors.red,
-      child: Text('hai'),
-    ),
+        appBar: buildAppBar(),
+        body: Column(
+          children: [
+            Container(width: width,height: height/15,margin: EdgeInsets.only(left:width/10,right: width/10),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              color: Colors.white,
+              
+            ),child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(padding:EdgeInsets.only(left: 5,right: 5),child: Icon(Icons.search)),
+                Text('Search')
+              ],
+            ),)
+          ],
+        ));
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: Colors.grey[300],
+      leading: Icon(Icons.menu, color: Colors.black),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Image.asset(
+            'assets/avatar.png',
+            width: 40,
+            height: 40,
+          ),
+        )
+      ],
     );
   }
 }
