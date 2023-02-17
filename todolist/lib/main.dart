7dea11f7b5ea2eb7e53ef5d7af42c15bbaa0cf6e
jalala-pv <pvjalala@gmail.com>
2023-02-17 12:32:24 +0530

    aaa

--- todolist/lib/main.dart
+++ todolist/lib/main.dart
@@ -13,21 +13,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
 
         primarySwatch: Colors.blue,
       ),
-      home:Home(),
+      home:TodoListPage(),
     );
   }
 }
-class Home extends StatelessWidget {
-  const Home({super.key});
+class TodoListPage extends StatelessWidget {
+  
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[300],
+        leading: Icon(Icons.menu,color: Colors.black),
+        actions: [Image.asset('assets/avatar.png',width: 40,height: 40,)],
+        
+      ),
     body: Container(
       color: Colors.red,
       child: Text('hai'),
